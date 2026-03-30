fc5414b1f210d8048d1ab8058d5e3e636705435d
mohamedsayah97 <mohamedsayah633@gmail.com>
2026-03-30 23:42:54 +0100

    fixing the updates

--- src/supplier/dtos/updateSupplier.dto.ts
+++ src/supplier/dtos/updateSupplier.dto.ts
@@ -1,4 +1,28 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateSupplierDto } from './createSupplier.dto';
+import { IsOptional, IsString, IsEmail, Length } from 'class-validator';
 
-export class UpdateSupplierDto extends PartialType(CreateSupplierDto) {}
+export class UpdateSupplierDto extends PartialType(CreateSupplierDto) {
+  @IsOptional()
+  @IsString()
+  @Length(2, 100)
+  supplierName?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(5, 255)
+  address?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(3, 50)
+  registrationNumber?: string;
+}
